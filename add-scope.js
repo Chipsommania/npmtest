7040b08ddecefe7f1f900746e54a56772c574430
Dragan Pavlovic <draganpavlovic@Dragans-MacBook-Pro.local>
2024-06-07 14:37:28 +0200

    Fix script

--- add-scope.js
+++ add-scope.js
@@ -2,15 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 const packagesDir = path.join(__dirname, 'packages');
-const scope = '@dragorog/strapi-plugins';
+const scope = '@dragorog';
 
 fs.readdirSync(packagesDir).forEach(packageName => {
   const packageJsonPath = path.join(packagesDir, packageName, 'package.json');
   if (fs.existsSync(packageJsonPath)) {
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-    if (packageJson.name.endsWith('-temp')) {
-      const pluginName = packageJson.name.replace('-temp', '');
-      packageJson.name = `${scope}/${pluginName}`;
+    if (!packageJson.name.startsWith(scope)) {
+      packageJson.name = `${scope}/${packageJson.name}`;
       fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
     }
   }

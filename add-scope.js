fe9ff770fa8625a72c399216ff333a9e08498cbd
Dragan Pavlovic <draganpavlovic@Dragans-MacBook-Pro.local>
2024-06-07 14:47:41 +0200

    Fix script

--- add-scope.js
+++ add-scope.js
@@ -10,10 +10,12 @@ fs.readdirSync(packagesDir).forEach(packageName => {
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
     if (!packageJson.name.startsWith(scope)) {
       const pluginName = packageJson.name.replace('-temp', '');
-      const version = packageJson.version;
-      packageJson.name = `${scope}-${pluginName}@v${version}`;
+      const version = packageJson.version.replace(/\./g, '-');
+      packageJson.name = `${scope}-${pluginName}-v${version}`;
       fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
       console.log(`Updated ${packageName}: ${packageJson.name}`);
     }
   }
 });
+
+

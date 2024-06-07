79fdbe793beabe1faa853c15a786cc76233d4f3e
Dragan Pavlovic <draganpavlovic@Dragans-MacBook-Pro.local>
2024-06-07 09:58:08 +0200

    fix

--- packages/npmone/index.js
+++ packages/npmone/index.js
@@ -1,1 +1,1 @@
-console.log("This is npaadsdsmdasdaasodsssssdsadasdsne dsdsdsddpackdasdsage. testingggg");
+console.log("This is npaadsdsmdasdaasodsssssdsadasdsne dsdsdsdaadpackdasdsage. testingggg");
